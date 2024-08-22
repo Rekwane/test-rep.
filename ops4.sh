75869be0dccc87e3b596be6de7a674200be965e4
Rekwane Wagner <rekwane@kali>
2024-08-22 18:37:26 -0500

    Fifth

---
+++ ops4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Lets create a function in bash that adds two number together
+# Stretch goal can you do subtraction, multipliaction or division
+# You will need to declare two variables
+
+a=1
+b=2
+sum=$((1+2))
+echo "The sum of $a and $b is $sum"
+
+c=3
+d=4
+dif=$((3-4))
+echo "The difference of $c and $d is $dif"
+
+e=5
+f=6
+prod=$((5*6))
+echo "The product of $e and $f is $prod"
