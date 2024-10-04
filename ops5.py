c582966b9d928f3905f1c68b349ee336b07d0303
Rekwane <rekwane2@gmail.com>
2024-10-03 21:28:38 -0500

    ops5py

---
+++ ops5.py
@@ -0,0 +1,13 @@
+# Write a program that works out whether if a given number is an 
+# odd or even number
+# 🚨 Don't change the code below 👇
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+number = int(input("enter a number: "))
+if number %  2 == 0:
+    print(f'("{number} is even")')
+else:
+    print(f'("{number} is odd")')
+    
